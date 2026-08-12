ddbe98cba85c1dfe187d89edf111317e5464224a
Fifi Laptop <ffojna36@gmail.com>
2026-08-12 18:35:37 +0200

    Utworzono poboczny pasek narzędzi w widoku wyników, gotowe do dodawania features

--- app/gui/results_view.py
+++ app/gui/results_view.py
@@ -1,7 +1,9 @@
 import customtkinter as ctk
+import tkinter as tk
 from PIL import Image
 
 from ..image.image_viewer import CTkImageViewer
+from ..utils import resource_path
 
 
 class ResultsView(ctk.CTkFrame):
@@ -17,6 +19,40 @@ class ResultsView(ctk.CTkFrame):
         
         
     def _build_ui(self):
+        self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=15)
         
-        self._viewer.pack(fill="both", anchor="center", expand=True)
+        self._viewer.grid(row=0, column=1, sticky="nsew")
+
+        self._button_pane = ctk.CTkFrame(self, bg_color="transparent")
+        self._button_pane.grid(row=0, column=0, sticky="ns")
+
+
+        recenter_image = Image.open(resource_path("assets/icons/recenter.png"))
+        recenter_ctkimage = ctk.CTkImage(light_image=recenter_image, dark_image=recenter_image, size=(40, 40))
+        _recenter_button = ctk.CTkButton(self._button_pane, 
+                                         width=50, height=50, 
+                                         image=recenter_ctkimage,
+                                         text=None,
+                                         command=self._on_recenter,
+                                         corner_radius=6)
+        _recenter_button.grid(row=0, column=0, sticky="ns", pady=6)
+
+        save_image = Image.open(resource_path("assets/icons/save.png"))
+        save_ctkimage = ctk.CTkImage(light_image=save_image, dark_image=save_image, size=(40, 40))
+        _save_button = ctk.CTkButton(self._button_pane, 
+                                     width=50, height=50, 
+                                     image=save_ctkimage,
+                                     text=None, 
+                                     command=self._on_save
+                                     corner_radius=6)
+        _save_button.grid(row=1, column=0, sticky="ns", pady=6)
+
+
+    def _on_save(self):
+        pass
+
+
+    def _on_recenter(self):
+        pass
